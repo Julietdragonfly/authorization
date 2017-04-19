39fe9469a7e2abc159914c1c9e81d5a67643550b
Julietdragonfly <jusyasya@list.ru>
2017-04-19 12:46:35 +0300

    Update configuration.php

--- www/application/core/configuration.php
+++ www/application/core/configuration.php
@@ -1,12 +1,12 @@
 <?php
 
-    define("SITE_PATH","fourth.pro");
+    define("SITE_PATH","namesite.pro");
 	class configuration {
 
 
 
         protected $libraries_path = 'application/libraries/';
-        protected $db_user = 'user';
+        protected $db_user = 'user_t';
         protected $db_password = '12345';
         protected $db_name = 'bd';
         protected $db_host = 'localhost';

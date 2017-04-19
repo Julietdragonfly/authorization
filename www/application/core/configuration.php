0682f91043d376385a556db538c3f42ee67350bf
Julietdragonfly <jusyasya@list.ru>
2017-04-19 12:46:10 +0300

    Update configuration.php

--- www/application/core/configuration.php
+++ www/application/core/configuration.php
@@ -6,8 +6,8 @@
 
 
         protected $libraries_path = 'application/libraries/';
-        protected $db_user = 'prodv30_musin';
+        protected $db_user = 'user';
         protected $db_password = '12345';
-        protected $db_name = 'lightit_test';
+        protected $db_name = 'bd';
         protected $db_host = 'localhost';
 	}
